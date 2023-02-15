36081db1f8afa5c5a0646deae5226477159398e9
Eber <52986565+doneber@users.noreply.github.com>
2023-02-14 20:05:08 -0400

    Refactor commands, add 'ls' command, details

--- main.js
+++ main.js
@@ -2,50 +2,71 @@
 const user = new URLSearchParams(window.location.search).get('user') || 'robot'
 
 function init() {
-  console.log('init');
-  document.querySelector('.user-terminal').innerText = `${user}@pc`
+  setTimeout(() => {
+    document.querySelector('.input-container').innerHTML =
+      `<span class="user-terminal">${user}@pc</span><span class="user-access">:<span class="user-path">~</span>$&nbsp; </span>
+    <input type="text" id="terminal-prompt" autofocus>`
+  }, 100)
 }
 
 init()
-
 
 // when user clicks on the terminal, focus on the input
 const terminal = document.querySelector('.terminal')
 terminal.addEventListener('click', () => {
   document.querySelector('#terminal-prompt').focus()
 })
+const commands = {
+  'clear': {
+    run: () => { document.querySelector('.history').innerHTML = '' },
+    description: ' - Clear the terminal screen.'
+  },
+  'help': {
+    run: () => {
+      // RENDER SOMETHING
+      const historyElement = document.querySelector('.history')
+      const newLineContainerElement = document.createElement('div')
+      newLineContainerElement.classList = 'history-line'
+      // iterates over the commands map and creates a new element for each command
+      newLineContainerElement.innerHTML = '<p>Available commands:</p>'
+      const newElement = document.createElement('p')
+      newLineContainerElement.appendChild(newElement)
+      // iterates commands and creates a new element for each command
+      Object.keys(commands).forEach((key) => {
+        const newElement = document.createElement('p')
+        newElement.innerHTML = '&nbsp; ' + key + ' ' + (commands[key].description || '')
+        newLineContainerElement.appendChild(newElement)
+      })
 
-const commands = new Map()
-commands.set('clear', () => { document.querySelector('.history').innerHTML = '' })
-
-commands.set('echo', (texts) => {
-  // RENDER SOMETHING
-  const historyElement = document.querySelector('.history')
-  const newElement = document.createElement('p')
-  const newLineContainerElement = document.createElement('div')
-  newLineContainerElement.classList = 'history-line'
-  newElement.innerText = texts.join(' ')
-  newLineContainerElement.appendChild(newElement)
-  historyElement.appendChild(newLineContainerElement)
-})
-commands.set('help', (texts) => {
-  // RENDER SOMETHING
-  const historyElement = document.querySelector('.history')
-  const newLineContainerElement = document.createElement('div')
-  newLineContainerElement.classList = 'history-line'
-  // iterates over the commands map and creates a new element for each command
-  newLineContainerElement.innerHTML = '<p>Available commands:</p>'
-  const newElement = document.createElement('p')
-  newLineContainerElement.appendChild(newElement)
-
-  commands.forEach((value, key) => {
-    const newElement = document.createElement('p')
-    newElement.innerHTML = '&nbsp; ' + key
-    newLineContainerElement.appendChild(newElement)
-  })
-
-  historyElement.appendChild(newLineContainerElement)
-})
+      historyElement.appendChild(newLineContainerElement)
+    },
+    description: ' - Display information about builtin commands.'
+  },
+  'echo': {
+    run: (texts) => {
+      // RENDER SOMETHING
+      const historyElement = document.querySelector('.history')
+      const newElement = document.createElement('p')
+      const newLineContainerElement = document.createElement('div')
+      newLineContainerElement.classList = 'history-line'
+      newElement.innerText = texts.join(' ')
+      newLineContainerElement.appendChild(newElement)
+      historyElement.appendChild(newLineContainerElement)
+    },
+    description: '[arg ...] - Prints the arguments'
+  },
+  'ls': {
+    run: () => {
+      // RENDER SOMETHING
+      const historyElement = document.querySelector('.history')
+      const newLineContainerElement = document.createElement('div')
+      newLineContainerElement.classList = 'history-line'
+      newLineContainerElement.innerHTML = '<p>index.html</p>'
+      historyElement.appendChild(newLineContainerElement)
+    },
+    description: ' - List directory contents'
+  }
+}
 
 function render(commandArr) {
 
@@ -66,24 +87,24 @@ function render(commandArr) {
 
 function runCommand() {
   const terminalPrompt = document.querySelector('#terminal-prompt')
-  const commandArr = terminalPrompt.value.trimStart().split(' ')
+  const commandArr = terminalPrompt.value.trimStart().trimEnd().split(' ')
 
   // assing first element to command and the rest to params
   const [command, params] = [commandArr[0], commandArr.slice(1)]
-  const validInput = commands.has(command)
-
   render(commandArr)
+  terminalPrompt.value = ''
 
-  if (!validInput) {
-    // RENDER SOMETHING
+  if (command === '')return
+  
+  if (commands[command]) {
+    commands[command].run(params)
+  } else {
+    // RENDER COMMAND NOT FOUND
     const messageElement = document.createElement('p')
     messageElement.innerText = `${command}: command not found. Try 'help'`
     document.querySelector('.history').appendChild(messageElement)
-  } else {
-    commands.get(command)(params)
   }
 
-  terminalPrompt.value = ''
 }
 
 window.addEventListener('keydown', event => {
